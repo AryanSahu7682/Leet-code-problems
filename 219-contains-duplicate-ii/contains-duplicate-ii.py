6d246b29c579f8997694350aaa397226c4b7626b
Aryan Sahu <aryansahu712007as@gmail.com>
2026-08-04 00:44:09 +0530

    Time: 37 ms (64.50%) | Memory: 25.6 MB (28.06%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        index_map={}
+        for i in range(len(nums)):
+            if nums[i] in index_map:
+                if i-index_map[nums[i]]<=k:
+                    return True
+            index_map[nums[i]]=i
+
+        return False             
